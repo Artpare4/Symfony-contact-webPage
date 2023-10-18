fb6e7cf3c4166338d7c8731c60a99a851b29886b
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-10-18 17:35:04 +0200

    Ajout de condition sur le paramètre times de manyTimes (entier)

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -24,7 +24,7 @@ class HelloController extends AbstractController
         return $this->render('hello/world.html.twig', ['name' => $name]);
     }
 
-    #[Route('/hello/{name}/{times}', name: 'app_hello_manytimes')]
+    #[Route('/hello/{name}/{times}', name: 'app_hello_manytimes', requirements: ['times' => '\d+'])]
     public function manyTimes(string $name, int $times)
     {
         return $this->render('hello/many_times.html.twig', ['name' => $name, 'times' => $times]);
