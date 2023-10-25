2279cad561e5fae0aa2406a5cfb76d7e859f8454
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-10-25 17:06:13 +0200

    Ajout du findBy

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(ContactRepository $contact): Response
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+        $res = $contact->findBy(['firstname' => 'ASC', 'lastname' => 'ASC']);
+
+        return $this->render();
     }
 }
