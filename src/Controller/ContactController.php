e1b891c73c1a3ee896aa30c064927aa6cdc94e15
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-10-26 10:36:32 +0200

    Ajut des url vers la description des contacts

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,7 +6,6 @@ use App\Entity\Contact;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -19,10 +18,9 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', ['contacts' => $res]);
     }
 
-    #[Route('/contact/{id}', requirements: ['contactId' => '\d+'])]
+    #[Route('/contact/{id}', name: 'app_contact_show', requirements: ['contactId' => '\d+'])]
     public function show(Contact $contact): Response
     {
-
         return $this->render('contact/show.html.twig', ['contact' => $contact]);
     }
 }
