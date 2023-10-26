6e9058381d917619348e66ac9f3c0ca8f12f6e95
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-10-26 10:43:36 +0200

    Ajout du test de vérification du lien vers la description du contact

--- tests/Controller/Contact/IndexCest.php
+++ tests/Controller/Contact/IndexCest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Tests\Controller\Contact;
 
 use App\Tests\Support\ControllerTester;
@@ -13,6 +12,14 @@ class IndexCest
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Liste des contacts');
         $I->see('Liste des contacts', 'h1');
-        $I->seeNumberOfElements('li', 195);
+        $I->seeNumberOfElements('a', 195);
+    }
+
+    public function checkRoadLink(ControllerTester $I): void
+    {
+        $I->amOnPage('/contact');
+        $I->seeResponseCodeIsSuccessful();
+        $I->click('Andre - Sébastien');
+        $I->seeCurrentRouteIs('app_contact_show');
     }
 }
