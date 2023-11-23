898f025adce586dcfef26bd2ab2c61039cf50a1d
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-11-23 08:55:52 +0100

    Ajout de la dépendance entre ContactFixtures et CategoryFixtures

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,5 +12,12 @@ class ContactFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         ContactFactory::createMany(150);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            Category::class,
+        ];
     }
 }
