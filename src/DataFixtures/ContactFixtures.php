1522aea3249eb1238aea82be603f79dce1c506ac
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-11-23 09:10:12 +0100

    Ajout d'une affectation aléatoire d'une catégorie à Contact

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Factory\CategoryFactory;
 use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,7 +12,17 @@ class ContactFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        ContactFactory::createMany(150);
+        ContactFactory::createMany(150, function () {
+            if (ContactFactory::faker()->boolean(90)) {
+                return [
+                  'category' => CategoryFactory::random(),
+                ];
+            } else {
+                return [
+                'category' => null,
+                ];
+            }
+        });
     }
 
     public function getDependencies(): array
