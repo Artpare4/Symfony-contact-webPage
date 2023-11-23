061043447f6d37de30e6c293b71b4b2e06d1bed2
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-11-23 08:52:02 +0100

    Modification du générateur de données de contact pour qu'il puisse généré à partir des données du fichier Category.json

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,12 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $data = json_decode(file_get_contents(__DIR__.'/data/Category.json'), true);
+        foreach ($data as $elmt) {
+            $catg = new Category();
+            $catg->setName($elmt['name']);
+            $manager->persist($catg);
+        }
 
         $manager->flush();
     }
