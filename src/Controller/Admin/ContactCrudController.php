28f419f2eebad3e4a31edf6263921149ff457f35
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-12-07 11:52:56 +0100

    Sauvegarde travail (question 23.5.6)

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -4,8 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Contact;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactCrudController extends AbstractCrudController
@@ -15,14 +16,16 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            EmailField::new('email'),
+            TextField::new('phone'),
+            AssociationField::new('cateogry.name')->setQueryBuilder(
+            ),
+            ];
     }
-    */
 }
